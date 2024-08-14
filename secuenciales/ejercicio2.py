071672d43cccf71f76e935ac61bf1500b36bb864
Andreysua <dansuarez243@gmail.com>
2024-08-14 22:42:14 +0200

    talle 1

---
+++ secuenciales/ejercicio2.py
@@ -0,0 +1,9 @@
+#Solicitar Los Datos de un Estudiante
+nomestudiante = input ("Ingrese el nombre"); #Digitar mediante el teclado y ingresar el nombre del estudiante
+nomcolegio = input ("Ingrese el nombre del colegio");#Digitar mediante el teclado y ingresar el nombre del estudiante
+grado = input ("Ingrese su grado");#Digitar mediante el teclado y ingresar el curso al cual están asistiendo
+#Imprimir la información del estudiante
+print("Información del estudiante"); #Procedencia sobre la información del estudiante en este caso nombre del estudiante, colegio y curso
+print("Nombre del estudiante", nomestudiante); #Información  sobre el nombre del estudiante
+print("Nombre del colegio", nomcolegio); #Información sobre el nombre del colegio al cual asistió el estudiantw
+print("grado que cursa", grado); #Información sobre el grado del estudiante durante el curso en el colegio
